77beba66c4f6a5cc9a765cd58dcb5d24bb34a661
kunal <kunalk8728@gmail.com>
2026-06-12 15:08:04 +0530

    building the navbar lmfaooo

--- frontend-netra/src/components/Navbar.jsx/Navbar.jsx
+++ frontend-netra/src/components/Navbar.jsx/Navbar.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import icon from '../../assets/icon.png'
+import './Navbar.css'
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <div className='flex items-center justify-between bg-red-600 py-20'>
+            <div className='flex items-center gap-4 '>
+                <img src={icon} alt="MOVINT icon" className='w-11 h-10 bg-black' />
+                <h2 className='text-white font-semibold text-2xl font-serif '>MOVINT</h2>
+            </div>
+            <div className='text-white font-medium text-[20px] flex items-center gap-3.5'>
+                <h3>Nationals</h3>
+                <h3>Deployment</h3>
+                <h3>Geo Tracker</h3>
+                <h3>Alerts</h3>
+                <h3>Liaison</h3>
+            </div>
+
+        </div>
+    )
 }
 
 export default Navbar
--- frontend-netra/src/pages/Interface.jsx
+++ frontend-netra/src/pages/Interface.jsx
@@ -4,7 +4,7 @@ import Navbar from '../components/Navbar.jsx/Navbar'
 
 const Interface = () => {
     return (
-        <div>
+        <div className='bg-gray-900 h-screen'> 
             <Navbar/>
         </div>
     )
