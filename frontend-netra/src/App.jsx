211aab947d4634f751af71a3589ef4c4a7f2da99
kunal <kunalk8728@gmail.com>
2026-06-12 14:19:15 +0530

    reviewing complete

--- frontend-netra/src/App.jsx
+++ frontend-netra/src/App.jsx
@@ -1,31 +1,13 @@
 import React, { useState } from 'react'
-import Entryform from './pages/Entryform'
-import Borderguard from './components/Borderguard'
-import Checkpost from './components/Checkpost'
+import Interface from './pages/Interface'
+
 
 const App = () => {
-  const [view, setView] = useState('entryform')
+  
 
   return (
-    <div>
-      {view === 'entryform' && (
-        <Entryform
-          onBorderGuardClick={() => setView('borderguard')}
-          onCheckpostClick={() => setView('checkpost')}
-        />
-      )}
-      {view === 'borderguard' && (
-        <Borderguard
-          onImmigrationClick={() => setView('entryform')}
-          onCheckpostClick={() => setView('checkpost')}
-        />
-      )}
-      {view === 'checkpost' && (
-        <Checkpost
-          onImmigrationClick={() => setView('entryform')}
-          onBorderGuardClick={() => setView('borderguard')}
-        />
-      )}
+    <div className=''>
+     <Interface/>
     </div>
   )
 }
---
+++ frontend-netra/src/pages/Interface.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import Navbar from '../components/Navbar.jsx/Navbar'
+
+const Interface = () => {
+    return (
+        <div>
+            <Navbar/>
+        </div>
+    )
+}
+
+export default Interface
