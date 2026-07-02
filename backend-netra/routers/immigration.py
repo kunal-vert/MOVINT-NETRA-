e18c822d4db8cb086a455565c9627e7ccff28a04
kunal <kunalk8728@gmail.com>
2026-07-02 12:59:48 +0530

    resolve merge conflict

---
+++ backend-netra/routers/immigration.py
@@ -0,0 +1,7 @@
+# This isn't the play role in DB directly here we will calc the risk score  consider parameters
+
+# Calling upon the Immigration Router only 
+
+HIGH_RISK_OCCUPATIONS = {"journalist", "researcher", "activist", "ngo worker", "photographer", "missionary", " War Veterans","Filmmaker"}
+
+HIGH_RISK_NATIONALITIES = {}
