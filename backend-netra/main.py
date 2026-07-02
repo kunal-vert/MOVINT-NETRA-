d8d82efe1d201e3806e3ee800f7bc28f8fa491c5
kunal <kunalk8728@gmail.com>
2026-07-02 20:39:39 +0530

    working on main.py

--- backend-netra/main.py
+++ backend-netra/main.py
@@ -1,41 +1,29 @@
-from fastapi import FastAPI, Request
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI
 
-app = FastAPI()
+from database import engine
+from model import Base
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
 
-templates = Jinja2Templates(directory="templates")
+# Create all tables
+Base.metadata.create_all(bind=engine)
 
-posts: list[dict] = [
-    {
-        "id": 1,
-        "author": "Kunal Pandit",
-        "title": "Urban warfare and Drone usuage",
-        "content": "The use of drones in urban wars has been so imperative on 21st centurey as tech have been developing.",
-        "date_posted": "April 20, 2026",
-    },
-    {
-        "id": 2,
-        "author": "Shreya Dubey",
-        "title": "Studyin law and Prep for UPSC CSE ",
-        "content": "All day i have to sit on desk and read so much sometime i just get exhuast .",
-        "date_posted": "April 27, 2026",
-    },
-]
+
+
+app = FastAPI(
+    title="MOVINT-NETRA",
+    description= "Counter Intelligence and Movement intellence",
+    version="1.0.0"
+)
+
+
+
 
 
 @app.get("/", include_in_schema=False, name="home")
-@app.get("/posts", include_in_schema=False, name="posts")
-def home(request: Request):
-    return templates.TemplateResponse(
-        request,
-        "home.html",
-        {"posts": posts, "title": "Home"},
-    )
+def home():
+    return{
+        "message":"MOVINT NETRA API running"
+    }
 
 
-@app.get("/api/posts")
-def get_posts():
-    return posts
+
