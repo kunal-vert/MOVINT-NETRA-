7c744f128c3d71f073e7a3a3693e127b2df6fdfe
kunal <kunalk8728@gmail.com>
2026-07-03 16:58:19 +0530

    Added the If cond checking the visitor

--- backend-netra/routers/immigration.py
+++ backend-netra/routers/immigration.py
@@ -30,7 +30,7 @@ def register_entry(
     )
 
 
-    past_visits = []    #this is for very high extreme case like he came to india and got register but never went to NE
+    past_visits = []   
     is_returning = False
     past_visits_count = 0
     
@@ -41,6 +41,17 @@ def register_entry(
         past_visits_count = len(past_visits)
 
 
+        existing.occupation       = data.occupation
+        existing.reason_to_visit  = data.reason_to_visit
+        existing.visa_type        = data.visa_type
+        existing.visa_permit_days = data.visa_permit_days
+        existing.visa_expiry      = data.visa_expiry
+        existing.criminal_record  = data.criminal_record
+        existing.prior_ne_visits  = past_visits_count
+
+        national = existing
+
+
 
     else:
         #Fresh NATIONAL creation as we have already checked in database that he/she wasn't present  
@@ -48,8 +59,9 @@ def register_entry(
         Visitor = ForeignNational(**data.model_dump())
         db.add(Visitor)
         db.flush() # assigns national.id without full commit
-          
 
-        
+        # here existing are information bout the subject/threat/visitors which would be database whereas the data are incoming info as fresh .Although info would be update if that person already been in NE 
 
+        # keeping in mind we don't have to change the name, passport_id dob .  Though in future we have to work on alias profile through ML/ AI :)..
 
+       
