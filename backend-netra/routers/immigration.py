808cfc8440e89b5c1ab6c2480cc589d8dc8405e0
kunal <kunalk8728@gmail.com>
2026-07-03 16:44:47 +0530

    Working on else conds to create Visitor profile

--- backend-netra/routers/immigration.py
+++ backend-netra/routers/immigration.py
@@ -30,8 +30,26 @@ def register_entry(
     )
 
 
-    past_visits = []
+    past_visits = []    #this is for very high extreme case like he came to india and got register but never went to NE
     is_returning = False
     past_visits_count = 0
-    national = None
     
+
+    if existing:
+        past_visits = existing.visits
+        is_returning = True
+        past_visits_count = len(past_visits)
+
+
+
+    else:
+        #Fresh NATIONAL creation as we have already checked in database that he/she wasn't present  
+
+        Visitor = ForeignNational(**data.model_dump())
+        db.add(Visitor)
+        db.flush() # assigns national.id without full commit
+          
+
+        
+
+
