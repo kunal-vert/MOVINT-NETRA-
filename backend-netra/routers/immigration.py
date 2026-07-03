5f057238ef3ab552199c6a32791ce400b43dcb2a
kunal <kunalk8728@gmail.com>
2026-07-03 10:08:05 +0530

    setting up fnx for checking passenger already exists

--- backend-netra/routers/immigration.py
+++ backend-netra/routers/immigration.py
@@ -9,3 +9,29 @@ from model import ForeignNational, VisitHistory
 from schemas import ImmigrationEntryRequest, ImmigrationEntryResponse
 from utils.risk import compute_risk
 
+
+
+router = APIRouter(prefix="/api/immgration", tags=["immigration"])
+
+
+
+@router.post("/entry", response_model=ImmigrationEntryResponse)
+def register_entry(
+    data: ImmigrationEntryRequest,
+    db: session = Depends(get_db)):
+
+
+    # we have to check whether threats already exists or not 
+
+    existing = (
+        db.query(ForeignNational)
+        .filter(ForeignNational.passport_id == data.passport_id)
+        .first()
+    )
+
+
+    past_visits = []
+    is_returning = False
+    past_visits_count = 0
+    national = None
+    
