845329277999271b5e4698150bf729225ff0c92f
kunal <kunalk8728@gmail.com>
2026-07-07 11:57:59 +0530

    creating instance of api

---
+++ frontend-netra/src/api/axiosclient.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+
+const api = axios.create({
+    baseURL:"",
+    timeout:5000,
+    headers:{
+        'content-type':'application/json'
+    }
+
+})
+
+export default api
