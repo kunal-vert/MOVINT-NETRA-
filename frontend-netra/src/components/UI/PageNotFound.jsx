61cc49239158c2ad9fc2d0bc1f30ed66ff2b85aa
kunal <kunalk8728@gmail.com>
2026-07-11 22:17:56 +0530

    fixing the pagenot found

--- frontend-netra/src/components/UI/PageNotFound.jsx
+++ frontend-netra/src/components/UI/PageNotFound.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const PageNotFound = () => {
   return (
-    <div>PageNotFound</div>
+    <div className='flex justify-center text-6xl font-[font1]  '>
+      <h1 className='text-shadow-blue-950'>PageNotFound</h1>
+    </div>
   )
 }
 
